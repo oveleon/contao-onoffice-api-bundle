9a1ae1f95216a1924aa180d92321a3cefc93eaba
Sebastian Zoglowek <sebastian@oveleon.de>
2022-11-04 08:42:54 +0100

    [Bugfix] Fixed some undefined array keys

--- src/Resources/contao/dca/tl_onoffice_api_view.php
+++ src/Resources/contao/dca/tl_onoffice_api_view.php
@@ -69,7 +69,7 @@ $GLOBALS['TL_DCA']['tl_onoffice_api_view'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_onoffice_api_view']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
             (
--- src/Resources/contao/dca/tl_onoffice_auth.php
+++ src/Resources/contao/dca/tl_onoffice_auth.php
@@ -60,7 +60,7 @@ $GLOBALS['TL_DCA']['tl_onoffice_auth'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_onoffice_auth']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
